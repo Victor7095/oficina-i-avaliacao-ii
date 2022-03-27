6101e40c86303506006d6e919211ec5f9b26ece8
NandaPL <fpl.snf19@uea.edu.br>
2022-03-27 14:37:55 -0400

    feat: loop de exibição de filmes assistidos e recomendados

--- interface/streamlit-interface.py
+++ interface/streamlit-interface.py
@@ -1,36 +1,56 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(layout="centered", page_icon="🎥", page_title="Movies App")
 
-# Primeira área de interação
-with st.container():
+def form_movie():
+    with st.form(key="form_movies"):
+        # Nome filme
+        st.write()
 
-    st.title("🎥")
-    html_title = """
-        <h2 style="color:white;text-align:center;">Sistema de Recomendação de Filmes</h2>
-    """
-    st.title("").markdown(
-        html_title, unsafe_allow_html=True)
+        # Avaliação
+        st.write()
 
-    html_temp = """
-        <div style="padding:10px">
-        <h3 style="color:white;text-align:center;">🤔 Quem vai assistir?</h3>
-        </div>
-    """
-    st.markdown(html_temp, unsafe_allow_html=True)
+        # Gênero
+        st.write()
 
-    test_user = st.text_input(
-        "Informe o ID do usuário que você deseja prevê as recomendações：")
+        # Ano
+        st.write()
 
-    col1, col2, col3 = st.columns([6, 3, 6])
+        # Sinopese
+        st.write(descricao)
 
-    col2.button("🔮Previsão🔮")
 
-st.markdown("")
+def main():
+    st.set_page_config(layout="centered", page_icon="🎥",
+                       page_title="Movies App")
 
-with st.container():
+    # Primeira área de interação
+    with st.container():
 
+        st.title("🎥")
+        html_title = """
+            <h2 style="color:white;text-align:center;">Sistema de Recomendação de Filmes</h2>
+        """
+        st.title("").markdown(
+            html_title, unsafe_allow_html=True)
+
+        html_temp = """
+            <div style="padding:10px">
+            <h3 style="color:white;text-align:center;">🤔 Quem vai assistir?</h3>
+            </div>
+        """
+        st.markdown(html_temp, unsafe_allow_html=True)
+
+        userId = st.text_input(
+            "Informe o ID do usuário que você deseja prevê as recomendações：")
+
+        col1, col2, col3 = st.columns([6, 3, 6])
+
+        predict = col2.button("🔮Previsão🔮")
+
+    st.markdown("")
+
+    # Segunda área de interação
     html_temp = """
             <div style=padding:15px">
             <h3 style="color:white;text-align:center;">✅ Assistidos</h3>
@@ -38,12 +58,30 @@ with st.container():
         """
     st.markdown(html_temp, unsafe_allow_html=True)
 
-st.markdown("")
+    with st.form(key="list_rating_movies"):
+        """ Percorre a lista de filmes assistidos pelo o usuário """
+        for i in range(len()):
+            """ Passar lista de filmes assistidos pelo usuário 
+            para captura dos dados referentes ao filme (nome, ano, gênero, etc.)"""
+            form_movie()
 
-with st.container():
+    st.markdown("")
+
+    # Terceira área de interação
     html_temp = """
             <div style=padding:10px">
             <h3 style="color:white;text-align:center;">👍 Recomendados</h3>
             </div>
         """
     st.markdown(html_temp, unsafe_allow_html=True)
+
+    with st.form(key="list_recommendation_movies"):
+        """ Percorre a lista de filmes recomendados ao usuário """
+        for i in range(len()):
+            """ Passar lista de filmes recomendados ao usuário 
+            para captura dos dados referentes ao filme (nome, ano, gênero, etc.)"""
+            form_movie()
+
+
+if __name__ == '__main__':
+    main()
